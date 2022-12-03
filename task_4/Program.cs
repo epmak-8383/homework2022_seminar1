8b0d41c964a77d26f943c6ae4b3aa1fcf8636694
Ermachenkov Maxim <epmak.8383@mail.ru>
2022-12-03 23:43:09 +0300

    задание 4

---
+++ task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int b = a - (a - 2);
+
+while (b <= a)
+{
+    Console.WriteLine(b);
+    b = b + 2;
+}
