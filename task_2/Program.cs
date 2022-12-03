63a31b2e55a9aa0d2542859d1a08a0a2cef85aba
Ermachenkov Maxim <epmak.8383@mail.ru>
2022-12-03 23:07:06 +0300

    задание 2

---
+++ task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+
+else 
+{   if (b > c)
+    {
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
